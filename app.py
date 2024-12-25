151814fbe58454dd98304ac3db56f614d5fbffb9
SHIVRAJ SHINDE <shindeshivraaj@gmail.com>
2024-12-25 16:23:40 +0530

    Add

--- app.py
+++ app.py
@@ -8,9 +8,12 @@ def sub(a, b):
 def mulumulllll(a, b):
     return a*b
 
+def mulumulllll02(a, b):
+    return a*b
 
-if __name__ == "__main__":
-    result1 = add(4, 5)
-    result2 = sub(4, 1)
 
-    print(result1, result2)
+
+result1 = add(4, 5)
+result2 = sub(4, 1)
+
+print(result1, result2)
